70ea9eed3c57febe656faeab03d40de3f693e5a4
Nissi Chima <nissichima@Nissis-MacBook-Pro.local>
2021-07-02 21:54:20 -0400

    fixed indentations in dev file

--- evaluate-news-nlp/webpack.dev.js
+++ evaluate-news-nlp/webpack.dev.js
@@ -14,8 +14,34 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
-        ]
+        ]}
     },
+    {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    },
+    {
+        test: /\.css$/,
+        use: [
+            {
+                loader: "style-loader",
+            },
+            {
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                },
+            },
+        ],
+    },
+    {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
@@ -31,4 +57,4 @@ module.exports = {
             protectWebpackAssets: false
         })
     ]
-}
+};
