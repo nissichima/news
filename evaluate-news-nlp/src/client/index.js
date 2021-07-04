6b7f935c72dacd5ba135c0c541a670cc01f0fe27
Nissi Chima <nissichima@Nissis-MacBook-Pro.local>
2021-07-04 11:43:40 -0400

    fixed         Field 'browser' doesn't contain a valid alias configuration error

--- evaluate-news-nlp/src/client/index.js
+++ evaluate-news-nlp/src/client/index.js
@@ -1,13 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 import './styles/resets.scss'
-/*
+
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/base.scss'
 import './styles/header.scss'
 import './styles/message.scss'
-*/
+
 /*
 export { checkUrl, handleSubmit };
 */
